ac0bbf03fa380ba696c8a50131096467c8792e12
Stephen Sherratt <stephen@sherra.tt>
2021-08-14 10:52:47 +1000

    Update nix shell

--- shell.nix
+++ shell.nix
@@ -1,10 +1,5 @@
-# Nix shell with rust compiler and dependencies for libraries and examples
-
 let
-  # This overlay is mozilla/nixpkgs-mozilla with the patch applied from this PR:
-  # https://github.com/mozilla/nixpkgs-mozilla/pull/250
-  # ...which replaces deprecated usage of stdenv.lib with lib.
-  moz_overlay_url = "https://github.com/gridbugs/nixpkgs-mozilla/archive/with-stdenv.lib-fix.tar.gz";
+  moz_overlay_url = "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz";
   moz_overlay = import (builtins.fetchTarball moz_overlay_url);
   nixpkgs = import <nixpkgs> {
     overlays = [ moz_overlay ];
@@ -18,11 +13,10 @@ stdenv.mkDerivation rec {
   name = "moz_overlay_shell";
   buildInputs = [
     ruststable
+    rust-analyzer
+    cargo-watch
   ];
 
   # Enable backtraces on panics
   RUST_BACKTRACE = 1;
-
-  # Without this graphical frontends can't find the GPU adapters
-  LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
 }
